9ed8b039def663e8a7136e3a8abe7c113911efc0
Prasannanjaneya <Prasannanjaneya.r@gmail.com>
2025-09-22 19:37:52 +0200

    Fix unsupported health_check_grace_period_seconds argument

--- terraform/modules/app-service/variables.tf
+++ terraform/modules/app-service/variables.tf
@@ -5,17 +5,17 @@ variable "prefix" {
 
 variable "resource_group_name" {
   type        = string
-  description = "Target Resource Group"
+  description = "Target Resource Group" #resource group name
 }
 
 variable "location" {
-  type        = string
-  default     = "EastUS"
+  type    = string
+  default = "EastUS"
 }
 
 variable "os_type" {
-  type        = string
-  default     = "Linux"
+  type    = string
+  default = "Linux"
   validation {
     condition     = var.os_type == "Linux" || var.os_type == "Windows"
     error_message = "os_type must be either 'Linux' or 'Windows'"
@@ -23,8 +23,8 @@ variable "os_type" {
 }
 
 variable "sku_name" {
-  type        = string
-  default     = "P1v3" # PremiumV3
+  type    = string
+  default = "P1v3" # PremiumV3
   validation {
     condition     = contains(["B1", "B2", "S1", "S2", "P1v3", "P2v3", "P3v3"], var.sku_name)
     error_message = "SKU must be one of: B1, B2, S1, S2, P1v3, P2v3, P3v3"
@@ -32,36 +32,36 @@ variable "sku_name" {
 }
 
 variable "node_version" {
-  type        = string
-  default     = "18-lts"
+  type    = string
+  default = "18-lts"
 }
 
 variable "always_on" {
-  type        = bool
-  default     = true
+  type    = bool
+  default = true
 }
 
 variable "health_check_path" {
-  type        = string
-  default     = "/healthz"
+  type    = string
+  default = "/healthz"
 }
 
 variable "app_settings" {
-  type        = map(string)
-  default     = {}
+  type    = map(string)
+  default = {}
 }
 
 variable "subnet_id" {
-  type        = string
-  default     = ""
+  type    = string
+  default = ""
 }
 
 variable "tags" {
-  type        = map(string)
-  default     = {}
+  type    = map(string)
+  default = {}
 }
 
 variable "custom_domain" {
-  type        = string
-  default     = null
+  type    = string
+  default = null
 }
